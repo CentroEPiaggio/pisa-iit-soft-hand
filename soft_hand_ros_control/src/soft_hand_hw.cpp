e46ba0daac69b0f42f4ea61ca1039f8f29f4a14a
Hamal Marino <hamal.marino@gmail.com>
2016-10-21 12:41:32 +0200

    corrected check for number of devices connected

--- soft_hand_ros_control/src/soft_hand_hw.cpp
+++ soft_hand_ros_control/src/soft_hand_hw.cpp
@@ -377,9 +377,9 @@ namespace soft_hand_hw
 
         ROS_DEBUG_STREAM( "Number of devices: " << aux_int );
 
-        if(aux_int > 1 || aux_int < 0)
+        if(aux_int > 1 || aux_int <= 0)
         {
-          ROS_WARN("The current port has more than one or none device connected... that is not a SoftHand");
+          ROS_WARN_STREAM("The current port has " << aux_int << " devices connected, but it must be only one... that is not a SoftHand");
         }
         else
         {
