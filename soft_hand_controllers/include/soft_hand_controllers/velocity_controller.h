3cf08429db925bf92c99e9fd51112bda68da05b2
George Jose Pollayil <gpollayil@gmail.com>
2018-10-05 12:13:34 +0200

    Created soft_hand_controllers package and added velocity controller.

---
+++ soft_hand_controllers/include/soft_hand_controllers/velocity_controller.h
@@ -0,0 +1,65 @@
+#ifndef SOFT_HAND_CONTROLLERS__VELOCITY_CONTROLLER_H
+#define SOFT_HAND_CONTROLLERS__VELOCITY_CONTROLLER_H
+
+#include <std_msgs/Float64.h>
+#include <ros/node_handle.h>
+#include <ros/ros.h>
+#include <urdf/model.h>
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_command_interface.h> 
+
+namespace soft_hand_controllers {
+
+    class VelocityController : public controller_interface::Controller<hardware_interface::PositionJointInterface> {
+
+        public:
+        
+        // Default Constructor
+        VelocityController();
+
+        // Default Destructor
+        ~VelocityController();
+
+        // Controller functions
+        bool init(hardware_interface::PositionJointInterface *robot, ros::NodeHandle &n);
+		void starting(const ros::Time& time);
+		void update(const ros::Time& time, const ros::Duration& period);
+		void command(const std_msgs::Float64::ConstPtr &msg);
+
+        private:
+
+        // A structure for joint limits of the soft hand
+        struct limits_ {
+			double min;
+			double max;
+			double center;
+		} joint_limits_;
+
+        // The joint name to be parsed from param server (from controllers.yaml)
+        std::string joint_name_;
+
+        // A URDF model of the robot and a joint
+        urdf::Model urdf_;
+        urdf::JointConstSharedPtr urdf_joint_;
+
+        // A joint handle
+        hardware_interface::JointHandle joint_handle_;
+
+        // Flag for position holding if cmd_value_ = 0
+        int cmd_flag_;
+
+        // Subscriber to the command topic of the controller
+        ros::Subscriber cmd_sub_;
+        ros::Time last_time_;             // Used to compute dt
+        ros::Time current_time_;          // Used to compute dt
+        ros::Duration dt_;                // The time resolution
+
+        // The current and commanded joint values
+        double curr_value_;
+        double cmd_value_;
+
+    };
+
+}
+
+#endif // SOFT_HAND_CONTROLLERS__VELOCITY_CONTROLLER_H
---
+++ soft_hand_controllers/src/velocity_controller.cpp
@@ -0,0 +1,98 @@
+#include <soft_hand_controllers/velocity_controller.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace soft_hand_controllers {
+
+    // DEFAULT CONSTRUCTOR
+    VelocityController::VelocityController(){
+        // Nothing to do
+    }
+
+    // DEFAULT DESTRUCTOR
+    VelocityController::~VelocityController(){
+        // Shutting down command subscriber
+        cmd_sub_.shutdown();
+    }
+
+    // INITIALIZING FUNCTION
+    bool VelocityController::init(hardware_interface::PositionJointInterface *robot, ros::NodeHandle &n){
+        // Getting the joint name from the parameter server
+        if(!n.getParam("joint", this->joint_name_)){
+            ROS_ERROR("Joint name not found in namespace: %s.", n.getNamespace().c_str());
+            return false;
+        }
+
+        // Parsing the URDF model
+        if(!this->urdf_.initParamWithNodeHandle("robot_description", n)){
+            ROS_ERROR("Failed to get the URDF from robot description!");
+            return false;
+        }
+
+        // Setting the URDF joint using the joint name and checking if it is present in the URDF model
+        this->urdf_joint_ = this->urdf_.getJoint(this->joint_name_);
+        if(!this->urdf_joint_){
+            ROS_ERROR("Could not find joint '%s' in the URDF.", this->joint_name_.c_str());
+        }
+
+        // Getting joint handle from hardware interface
+        this->joint_handle_ = robot->getHandle(this->joint_name_);
+
+        // Getting and setting the joint limits
+        this->joint_limits_.min = this->urdf_joint_->limits->lower;
+        this->joint_limits_.max = this->urdf_joint_->limits->upper;
+        this->joint_limits_.center = (this->joint_limits_.max + this->joint_limits_.min) / 2;
+
+        // Initializing the cmd_flag_
+        cmd_flag_ = 0;
+
+        // Initializing subscriber to command topic
+        this->cmd_sub_ = n.subscribe<std_msgs::Float64>("command", 1, &VelocityController::command, this);
+
+        return true;
+    }
+
+    // STARTING FUNCTION
+	void VelocityController::starting(const ros::Time& time){
+        // Getting previous time
+        last_time_ = time;
+
+        // Setting desired twist to zero
+        cmd_value_ = 0.0;
+
+        // Reading the joint state (position) and setting command
+        this->curr_value_ = this->joint_handle_.getPosition();
+        this->cmd_value_ = this->curr_value_;
+        this->joint_handle_.setCommand(cmd_value_);
+    }
+
+    // UPDATING FUNCTION
+	void VelocityController::update(const ros::Time& time, const ros::Duration& period){
+        // THE CODE INSIDE NEXT IF EXECUTED ONLY IF cmd_value_ != 0
+        if(cmd_flag_){
+            
+            // Getting current time resolution and updating last_time_
+            current_time_ = time;
+            dt_ = current_time_ - last_time_;
+            last_time_ = current_time_;
+
+            // Reading the joint state
+            this->curr_value_ = this->joint_handle_.getPosition();
+
+            // Setting command to current position (if cmd_flag_ = 0, position hold)
+            this->cmd_value_ = this->curr_value_;
+
+            
+        }
+    }
+
+    // COMMAND SUBSCRIBER CALLBACK
+	void VelocityController::command(const std_msgs::Float64::ConstPtr &msg){
+        // Saving the msg to cmd_value_
+        this->cmd_value_ = double (msg->data);
+
+        // Setting cmd_flag_ according to the commanded value
+        if(cmd_value_ == 0.0) cmd_flag_ = 0;
+        else cmd_flag_ = 1;
+    }
+
+}
